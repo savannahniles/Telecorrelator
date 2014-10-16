ebfc7652d08a342686e44bd382f417717f21f654
Savannah Niles <savannahniles@gmail.com>
2014-10-16 14:15:48 -0400

    changed date again

--- telecorrelator.py
+++ telecorrelator.py
@@ -66,7 +66,7 @@ def getContent():
 
     for trend in trends:
         UM_QUERY_API = "http://um-query.media.mit.edu/search/"
-        url = UM_QUERY_API + urllib2.quote(trend.encode('utf-8')) + "?segmentType=scene" + "&after=2014-09-01T00:00:00.000Z"
+        url = UM_QUERY_API + urllib2.quote(trend.encode('utf-8')) + "?segmentType=scene" + "&after=2014-10-09T00:00:00.000Z"
         umQueryResponse = json.loads(urllib2.urlopen(url).read())
         if umQueryResponse["code"] == 0:
             results = umQueryResponse["results"]
